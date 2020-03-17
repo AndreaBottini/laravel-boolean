aadb94dc35c79377823b5a30557e747645b9ace2
Andrea Bottini <andrea.bottini22@gmail.com>
2020-03-17 20:14:02 +0100

    Schede Dipendenti

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,8 +1,14 @@
 @extends('layouts.layout')
 
 @section('main')
-@foreach (config('students.students') as $student)
-    {{$student['nome']}}
-@endforeach
-    
+<div class="students">
+    @foreach (config('students.students') as $student)
+        <div class="student">
+            <img src="{{$student['img']}}" alt="">
+            <h2>{{$student['nome']}} ({{$student['eta']}} anni )</h2>
+            <h3>Assunt{{ ($student['genere'] == 'm')? 'o' : 'a'}} da {{$student['azienda']}} come  {{$student['ruolo']}}</h3>
+            <p>{{$student['descrizione']}}</p>
+        </div>
+    @endforeach
+</div>    
 @endsection
