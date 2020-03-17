71ff83784c7201d5193c3c646e54f155f7c0f6ee
Andrea Bottini <andrea.bottini22@gmail.com>
2020-03-17 16:07:01 +0100

    Creazione Controller e collegamento

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//index
+Route::get('/', 'ControllerHome@home')->name('Homepage');
