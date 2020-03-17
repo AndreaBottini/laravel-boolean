c70d1e409947e500ee317c83d3e1d9c59d4e9fee
Andrea Bottini <andrea.bottini22@gmail.com>
2020-03-17 16:49:42 +0100

    Aggiunti partials-layouts ed include ed extends

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,14 @@
+@include('layouts.partials._header')
+
+    <div class="container">
+        <header>
+
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
+
+@include('layouts.partials._footer')
