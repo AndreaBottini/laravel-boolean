de8188bdfe23684becc66598f7857c33913eae3a
Andrea Bottini <andrea.bottini22@gmail.com>
2020-03-20 17:06:32 +0100

    mofdifica env, creazione model, prima migration e rollback e nuova migration

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -35,3 +35,19 @@
                 </li>
             </ul>
         </nav>
+        @section('main')
+            <div class="jumbo">
+                <div class="jumbo-info">
+                    <h1>DIVENTA SVILUPPATORE WEB</h1>
+                    <h2>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</h2>
+                    <ul>
+                        <li>6 mesi di corso intensivo online in diretta</li>
+                        <li>Nessuna competenza di programmazione richiesta</li>
+                        <li>Siamo certi del tuo successo, altrimenti ti rimborsiamo</li>
+                    </ul>
+                </div>
+                <div class="img-computer">
+                    <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif" alt="">
+                </div>
+            </div>    
+        @endsection
